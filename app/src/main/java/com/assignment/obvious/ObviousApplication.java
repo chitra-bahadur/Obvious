03adcbf7fb7de3b3e96c3d4e4297f3bf8b12e9e2
Chitra Bahadur <chitrabahadur@Chitras-MacBook-Air.local>
2020-02-03 22:41:35 +0530

    Dagger2 implementations
    Created AppComponent
    Dagger creates depdency graph so it can search and find out the dependency when needed. To do this we needed an interface annotated with @component
    
    Implementing Dependency Injection using dagger2. Advantages of DependecyInjections are resuability, ease of testing.
    Benfits of Dagger2
    -> creates factories for the classes available in graph. no need to create manually.
    -> re-using a dependency
    -> creating containers for specific flow
    -> Generating Application Graph

---
+++ app/src/main/java/com/assignment/obvious/ObviousApplication.java
@@ -0,0 +1,20 @@
+package com.assignment.obvious;
+
+import android.app.Application;
+
+import com.assignment.obvious.di.component.ApplicationComponent;
+import com.assignment.obvious.di.component.DaggerApplicationComponent;
+
+//app component lives in the application class to share its life cycle
+public class ObviousApplication extends Application {
+
+    ApplicationComponent appComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Reference to the application graph that is used across the whole app
+        appComponent = DaggerApplicationComponent.create();
+    }
+}
---
+++ app/src/main/java/com/assignment/obvious/di/component/ApplicationComponent.java
@@ -0,0 +1,7 @@
+package com.assignment.obvious.di.component;
+
+import dagger.Component;
+
+@Component
+public interface ApplicationComponent {
+}
